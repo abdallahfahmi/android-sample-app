b4ef85eef7ba25105850ce80e0c0599108455e7e
abdullahfahmi <afahmi@identity-solutions.org>
2021-12-24 10:47:18 +0200

    setup action bar with navigation controller

--- app/src/main/java/com/fawry/task/ui/main/MainActivity.kt
+++ app/src/main/java/com/fawry/task/ui/main/MainActivity.kt
@@ -2,13 +2,33 @@ package com.fawry.task.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.fawry.task.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBar()
     }
+
+    private fun setupActionBar() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.mainHostFragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
